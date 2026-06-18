50f43270624169e8d8b804a0e15b292043267238
Ben Schmitz <ben.schmitz@condero.com>
2026-06-18 12:31:51 +0200

    fixed file.py

--- file.py
+++ file.py
@@ -1,12 +1,12 @@
 def get_file_content(path:str,mode:str = "r") -> str:
-    with open(path,mode), as f:
+    with open(path,mode) as f:
         return f.read() 
 
 def get_file_lines(path:str,mode:str = "r") -> str:
-    with open(path,mode), as f:
+    with open(path,mode) as f:
         return f.readlines() 
 
-def set_file_content(path:str,mode:str = "w",data:str) -> None:
-    with open(path,mode), as f:
+def set_file_content(path:str,mode:str = "w",data:str="") -> None:
+    with open(path,mode) as f:
         return f.write(data) 
 
